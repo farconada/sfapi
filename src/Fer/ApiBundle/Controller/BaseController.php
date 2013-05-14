828e3608f03208f128ce39175a8c231473d0df1c
Fernando A <fernando.arconada@gmail.com>
2013-05-14 22:00:31 +0200

    renombrado el api y añadida una nueva con objetos complejos

--- src/Fer/ApiBundle/Controller/BaseController.php
+++ src/Fer/ApiBundle/Controller/BaseController.php
@@ -10,18 +10,41 @@
 namespace Fer\ApiBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 
-class BaseController extends FOSRestController {
+class Model {
+    public $nombre;
+    public $apellido;
+    public $fecha;
+    public $miarray;
+}
 
-    public function getAction($name)
-    {
-        $view = $this->view(array('nombre' => $name), 200);
-        return $this->handleView($view);
-    }
+class BaseController extends FOSRestController {
 
     public function postAction()
     {
         $value = $this->getRequest()->request->get('value','no hay parametro');
         $view = $this->view(array('enviado' => $value), 200);
+        return $this->handleView($view);
+    }
+
+    public function getComplejoAction()
+    {
+        $obj = new Model();
+        $obj->nombre = 'Fer';
+        $obj->apellido = 'Ape';
+        $obj->fecha = new \DateTime();
+        $data = array();
+        array_push($data, $obj);
+        $obj2 = clone $obj;
+        $obj2->nombre = 'Alb';
+        $obj2->miarray = array('a', 'b');
+        array_push($data,$obj2);
+        $view = $this->view($data, 200);
+        return $this->handleView($view);
+    }
+
+    public function getNombreAction($name)
+    {
+        $view = $this->view(array('nombre' => $name), 200);
         return $this->handleView($view);
     }
 }
