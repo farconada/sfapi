91e9ea6d3955593137d5f8f1a6069c1356fe6483
Fernando A <fernando.arconada@gmail.com>
2013-05-20 22:39:44 +0200

    GET y POST de mensajes

--- src/Fer/ApiBundle/Controller/BaseController.php
+++ src/Fer/ApiBundle/Controller/BaseController.php
@@ -8,7 +8,10 @@
  */
 
 namespace Fer\ApiBundle\Controller;
+use Fer\ApiBundle\Entity\Mensaje;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use Symfony\Component\HttpFoundation\Request;
 
 class Model {
     public $nombre;
@@ -46,5 +49,44 @@ class BaseController extends FOSRestController {
     {
         $view = $this->view(array('nombre' => $name), 200);
         return $this->handleView($view);
+    }
+
+    /**
+     * @RequestParam(name="mensaje")
+     * @param  Request $request
+     */
+    public function postMensajeAction(Request $request){
+        $msgParam = $this->getRequest()->request->get('mensaje',array());
+        $msg = new Mensaje();
+        $msg->setFecha(new \DateTime());
+        $msg->setTexto($msgParam['texto']);
+        $msg->setUser($this->getLoggedUser());
+        $this->getEntityManager()->persist($msg);
+        $this->getEntityManager()->flush();
+        $view = $this->view(array('msg' => 'hecho'), 200);
+        return $this->handleView($view);
+    }
+
+    public function getMensajesAction(){
+        $mensajesRepository = $this->getRepository('FerApiBundle:Mensaje');
+        $mensajes = $mensajesRepository->findAll();
+
+        $view = $this->view($mensajes, 200);
+        return $this->handleView($view);
+    }
+
+    public function getRepository($repository)
+    {
+        return $this->getDoctrine()->getRepository($repository);
+    }
+
+    public function getEntityManager()
+    {
+        return $this->getDoctrine()->getManager();
+    }
+
+    public function getLoggedUser()
+    {
+        return $this->get('security.context')->getToken()->getUser();
     }
 }
