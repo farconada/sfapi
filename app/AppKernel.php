47e8cf659fe8dfc7ec2b774299d829cf15fb25fd
Fernando A <fernando.arconada@gmail.com>
2013-05-20 22:03:56 +0200

    controller WSSE

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
 	        new FOS\FacebookBundle\FOSFacebookBundle(),
+            new Escape\WSSEAuthenticationBundle\EscapeWSSEAuthenticationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Fer/ApiBundle/Controller/WsseController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: fernando
+ * Date: 20/05/13
+ * Time: 21:09
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Fer\ApiBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\NamePrefix;
+
+/**
+ * @NamePrefix("api_wsse_")
+ */
+class WsseController extends BaseController {
+
+}
